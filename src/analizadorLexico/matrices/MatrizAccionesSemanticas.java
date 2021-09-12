6aeb6da4e79ea8c5977b2bddf6517f802aa4a5c7
Martín Visca <mvisca@technisys.com>
2021-09-12 19:25:29 -0300

    Agregada la clase para manejar la estructura de la matriz de acciones semánticas

---
+++ src/analizadorLexico/matrices/MatrizAccionesSemanticas.java
@@ -0,0 +1,23 @@
+package analizadorLexico.matrices;
+
+import accionSemantica.AccionSemantica;
+
+@SuppressWarnings("all")
+public class MatrizAccionesSemanticas {
+
+    private AccionSemantica[][] matriz;
+
+    public MatrizAccionesSemanticas() {}
+
+    public MatrizAccionesSemanticas(int estadosOrigen, int simbolos) {
+        this.matriz = new AccionSemantica[estadosOrigen][simbolos];
+    }
+
+    public AccionSemantica get(int estadoOrigen, int simbolo) {
+        return matriz[estadoOrigen][simbolo];
+    }
+
+    public void set(int estadoOrigen, int simbolo, AccionSemantica accionSemantica) {
+        matriz[estadoOrigen][simbolo] = accionSemantica;
+    }
+}
